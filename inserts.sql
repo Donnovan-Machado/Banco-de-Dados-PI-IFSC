4f42e04f26a599e2e50bb4bd3f7a73a92c4bd9d9
Douglas E. K <douglas.k2005@aluno.ifsc.edu.br>
2024-05-07 11:23:09 -0300

    produtos-insert
    k

--- inserts.sql
+++ inserts.sql
@@ -1,4 +1,5 @@
-INSERT into Clientes(nome, email) values (?,?)
+INSERT into Clientes(nome, email, cpf) values (?,?,?)
 INSERT into Endereco(CEP, rua, bairro, complemento) values (?, ?, ?, ?)
 Insert Into Funcionarios (cpf, senha, nome) values (?, ?, ?);
 INSERT into Promocoes(idPromocoes, desconto, Produtos_idProdutos) values (?, ?, ?)
+insert into produtos (material, valor, categoria, estoque, tamanho) values (?, ?, ?, ?, ?);
