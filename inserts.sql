9aaa1c62e1bf90aa83f9d8f00f9e8b00e5e88922
Donnovan Machado <donnovanriddle19@gmail.com>
2024-05-07 11:10:47 -0300

    insert
    promoções

--- inserts.sql
+++ inserts.sql
@@ -1,2 +1,3 @@
 INSERT into Clientes(nome, email) values (?,?)
 INSERT into Endereco(CEP, rua, bairro, complemento) values (?, ?, ?, ?)
+INSERT into Promocoes(idPromocoes, desconto, Produtos_idProdutos) values (?, ?, ?)
