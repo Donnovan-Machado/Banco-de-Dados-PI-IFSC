85c77224506f1d8645819d421ebf61db175cae54
Cadu <carlos.esf@aluno.ifsc.edu.br>
2024-05-13 10:07:38 -0300

    Integração do Join
    Criação de onde devem estar os joins

--- selects.sql
+++ selects.sql
@@ -8,3 +8,24 @@ select * from Funcionarios;
 select * from Promocoes;
 
 select * from Funcionarios where senha = ? and nome = ?;
+
+//--Vendas-\\
+
+Select * FROM vendas AS vendas
+INNER JOIN clientes AS c 
+ON
+c.Clientes_cpf = v. Clientes_cpf
+INNER JOIN Funcionarios AS f
+ON f.cpf = v.cpf;
+
+//--Promocoes--\\
+
+//--Produtos--\\
+
+//--Carrinho--\\
+
+//--Fornecedores--\\
+
+//--Vendas--\\
+
+
