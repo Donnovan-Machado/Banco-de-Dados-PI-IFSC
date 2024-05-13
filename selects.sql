882c9a3c23090ef81a860fc0d6b895f8e81c3047
Cadu <carlos.esf@aluno.ifsc.edu.br>
2024-05-13 11:22:20 -0300

    Corrigir o JOIN INNER

--- selects.sql
+++ selects.sql
@@ -9,27 +9,21 @@ select * from Promocoes;
 
 select * from Funcionarios where senha = ? and nome = ?;
 
-//--Vendas-\\
+#--Corrigir daqui para baixo--#
 
 Select * FROM vendas AS v
 INNER JOIN clientes AS c 
 ON
-c.Clientes_cpf = v. Clientes_cpf
+c.cpf = v. Clientes_cpf
 INNER JOIN Funcionarios AS f
 ON f.cpf = v.cpf;
 
-//--Promocoes--\\
-SELECT * FROM  Promocoes AS p
 
-INNER JOIN Produtos_idProdutos AS pid
-ON p.
-
-//--Produtos--\\
 select * from Produtos as p 
 inner join fornecedores as f on Produtos 
 f.idFornecedores = p.Fornecedores_idFornecedores 
 
-//--Carrinho--\\
+
 select * from Carrinho as c 
 inner join vendas as v on Carrinho
 v.idVendas = c.Vendas_idVendas
@@ -37,7 +31,7 @@ v.idVendas = c.Vendas_idVendas
 inner join Produtos as p on Carrinho
 p.idProdutos = c.Produtos_idProdutos
 
-//--Fornecedores--\\
+
 select * from fornecedores as f 
 inner join Endereco as e on fornecedores
 e.CEP = f.Endereco.CEP
